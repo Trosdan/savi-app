e182ed09818b802d01fa0ac98f0861f3d4fbbf90
Davi William Moraes Suga <33470530+daviwms999@users.noreply.github.com>
2019-11-07 11:14:24 -0300

    Finalized offer list development.

--- src/components/OffersList.js
+++ src/components/OffersList.js
@@ -1,51 +1,75 @@
 import React, { Component } from "react";
-import { Text } from "react-native";
-import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
-import { graphql } from "react-apollo";
 import gql from "graphql-tag";
+import { useQuery } from '@apollo/react-hooks';
+import {Text} from 'react-native-paper'
+import {StyleSheet} from 'react-native'
+import { Avatar, Button, Card, Title, FAB} from 'react-native-paper';
 
-class OffersList extends Component {
-  render() {
-    console.log(this.props.data.offers.results);
-    return (
-      <>
-        {this.props.data.offers.results.map(results => (
-          <Text>{results.id}</Text>
-        ))}
-      </>
-    );
-  }
-}
+export default OffersList = ({ navigation }) => {
 
-let organizarionName = "Makaia";
-
-const OffersQuery = gql(`query {
-  offers(where: { name: { equalTo: "${organizarionName}" } }) {
-    results {
-      id
-      offerResponsable
-      nursery
-      shelter
-      clothes
-      developmentArea
-      education
-      name
-      local {
-        id
-      }
-    } 
-  }
-}
-  `);
-
-export default graphql(OffersQuery, {
-  options: {
-    context: {
-      headers: {
-        "X-Parse-Application-Id": "",
-        "X-Parse-Master-Key": "",
-        "X-Parse-Client-Key": ""
+  const styles = StyleSheet.create({
+    fab: {
+      position: 'absolute',
+      margin: 16,
+      right: 0,
+      bottom: 0,
+    },
+    card: {
+      margin: '10px'
+    }
+  })
+  let organizarionName = "Makaia";
+  const OffersQuery = gql(`
+      query {
+      offers(where: { name: { equalTo: "${organizarionName}" } }) {
+        results {
+          id
+          offerResponsable
+          nursery
+          shelter
+          clothes
+          developmentArea
+          education
+          name
+          local {
+            id
+          }
+        } 
       }
     }
+  `);
+
+  const { data, loading, error } = useQuery(OffersQuery);
+  if (loading) return <Text>Loading...</Text>;
+  if (error) {
+    console.log(error)
+    return <Text>ERROR :/</Text>
   }
-})(OffersList);
+  return (
+    <>
+      {data.offers.results.map(results => (
+         <>
+        <Title>Ofertas</Title>
+        <Card >
+          <Card.Title title={results.name} subtitle={results.developmentArea} left={(props) => <Avatar.Icon {...props} icon="folder" />} />
+
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+          <Card.Actions>
+            <Button>Editar oferta</Button>
+          </Card.Actions>
+        </Card>
+        <FAB 
+            style={styles.fab}
+            
+            icon="plus"
+            onPress={() => navigation.navigate("AddOffer")}/>
+
+        </>
+      ))}
+    </>
+  );
+}
+
+
+
+
