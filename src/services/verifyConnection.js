71dfefcf5584149774008f8f4273fc3b87589d83
Davi Suga <daviciencia1@gmail.com>
2020-02-07 13:10:47 -0300

    improvement(verifyconnection): verifyConnection function now returns true or false

--- src/services/verifyConnection.js
+++ src/services/verifyConnection.js
@@ -1,5 +1,9 @@
-export const verifyConnection = async () => {
-    response = await fetch("https://ifconfig.me/");
-    console.log("connection status: ", response.status);
-    return response.status;
-};
+export default async function verifyConnection() {
+    try {
+        let response = await fetch("www.google.com");
+        console.log("connection status: ", response.status);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
